796af148fa04011c5adc66d9ef19e68c6a6f6ceb
Filipe Pacheco da Silva Avelino <filipepacheco@uninove.br>
2020-10-20 20:01:01 -0300

    as

--- controle-salas.js
+++ controle-salas.js
@@ -5,6 +5,7 @@ var sala1TituloMenu = 'Nome da Palestra 1';
 var sala1FormPerguntas = 'https://docs.google.com/forms/d/e/1FAIpQLSeAfywC12xHNXHawLyLMnPgu1kHpjswNTUbhVzDhd7UwKAzPg/viewform?usp=sf_link';
 var sala1FormPresenca = 'https://docs.google.com/forms/d/e/1FAIpQLSeJW8qqJpXfggFZG3JfeOrLB7FtIeXv9b3FeuE0aNfyFZjd3w/viewform?usp=sf_link';
 //var sala1Descricao = 'Filipe';
+var sala1VideoID = '-RGRnv2Je5U';
 
 // Sala 2:
 var sala2Bandeira = 'us';
@@ -13,6 +14,7 @@ var sala2TituloMenu = sala1TituloMenu;
 var sala2FormPerguntas = sala1FormPerguntas;
 var sala2FormPresenca = sala1FormPresenca;
 //var sala2Descricao = 'descrição Sala 2';
+var sala2VideoID = '-RGRnv2Je5U';
 
 // Sala 3:
 var sala3Bandeira = 'br';
@@ -21,6 +23,7 @@ var sala3TituloMenu = 'Apresentação de trabalhos científicos';
 var sala3FormPerguntas = 'https://docs.google.com/forms/d/e/1FAIpQLSdqsWo2N53KzSV1v9PPIWXPWPxe8aWlKJWZcSCy4UW3JMoPQw/viewform?usp=sf_link';
 var sala3FormPresenca = 'https://docs.google.com/forms/d/e/1FAIpQLScVui0JiHJ627tR1WmXphk1OcwcODQBVEVzHk1-xFbxh0UxbA/viewform?usp=sf_link';
 //var sala3Descricao = 'descrição Sala 3';
+var sala3VideoID = '-RGRnv2Je5U';
 
 // Sala 4:
 var sala4Bandeira = 'us';
@@ -29,6 +32,7 @@ var sala4TituloMenu = 'Hall dos Patrocinadores';
 var sala4FormPerguntas = sala3FormPerguntas;
 var sala4FormPresenca = sala3FormPresenca;
 //var sala4Descricao = 'descrição Sala 4';
+var sala4VideoID = '-RGRnv2Je5U';
 
 // Sala 4:
 var sala5Bandeira = 'br';
@@ -104,12 +108,6 @@ function linkPresenca(link) {
 };
 
 // -------------------------------------------------------------------------------
-
-
-
-
-
-
 
 
 
